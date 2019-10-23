669d6a121dfb88edbf9ca69b0d4a5f44c2ca8b61
chengpengfei <chengpengfei@fangdd.com>
2019-10-23 16:21:04 +0800

    alinode change

--- docker.esf.fangdd.net/alinode-alpine/start-agenthub.sh
+++ docker.esf.fangdd.net/alinode-alpine/start-agenthub.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
+echo '当前环境是:'$NODE_ENV
+node /default.config.js
 
-ENABLE_NODE_LOG=NO node /default.config.js
-
-echo "current home is : $HOME"
-
-if [ -f $HOME/agenthub-running.json ]; then
-  cat $HOME/agenthub-running.json
-
-  # exec
-  ENABLE_NODE_LOG=NO agenthub $HOME/agenthub-running.json &
+if [[ -f $HOME/agenthub-running.json && $NODE_ENV = production ]]
+then
+  ENABLE_NODE_LOG=YES agenthub $HOME/agenthub-running.json 
+else
+  echo '[注意]：只有在生产环境才会开启 alinode '
 fi
 
 exec "$@"
